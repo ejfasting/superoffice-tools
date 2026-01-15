30620952a10bb33e8d310b91fd0d472d1b08eb42
Eivind Fasting <eivind.fasting@superoffice.com>
2026-01-15 22:30:33 +0100

    First commit with base project setup using pnpm, oxlint and simple typescript vscode extension

---
+++ packages/vscode/src/index.ts
@@ -0,0 +1,13 @@
+
+import * as vscode from 'vscode';
+import { versionStr, max } from './data';
+import { randomInt } from 'node:crypto';
+
+export function activate(_: vscode.ExtensionContext) {
+    console.log("test: activate called");
+	vscode.commands.registerCommand('dummy.helloWorldTs', () => {
+
+        const temp = `[${randomInt(max)}] Hello World to ${versionStr}! I am here! ddd`;
+		vscode.window.showInformationMessage(temp);
+	});
+}
