f5de2c364bfcdc279b9fd7602584d29b19f5993b
Eivind Fasting <eivind.fasting@superoffice.com>
2026-03-13 23:46:30 +0100

    fixed formatting/fmt

--- packages/language/src/generated/module.ts
+++ packages/language/src/generated/module.ts
@@ -3,36 +3,52 @@
  * DO NOT EDIT MANUALLY!
  ******************************************************************************/
 
-import type { LangiumSharedCoreServices, LangiumCoreServices, LangiumGeneratedCoreServices, LangiumGeneratedSharedCoreServices, LanguageMetaData, Module } from 'langium';
-import { CrmscriptAstReflection } from './ast.js';
-import { CrmscriptDefinitionGrammar, CrmscriptImplementationGrammar } from './grammar.js';
+import type {
+  LangiumSharedCoreServices,
+  LangiumCoreServices,
+  LangiumGeneratedCoreServices,
+  LangiumGeneratedSharedCoreServices,
+  LanguageMetaData,
+  Module,
+} from "langium";
+import { CrmscriptAstReflection } from "./ast.js";
+import { CrmscriptDefinitionGrammar, CrmscriptImplementationGrammar } from "./grammar.js";
 
 export const CrmscriptDefinitionLanguageMetaData = {
-    languageId: 'crmscript-definition',
-    fileExtensions: ['.crmscript-definition'],
-    caseInsensitive: false,
-    mode: 'development'
+  languageId: "crmscript-definition",
+  fileExtensions: [".crmscript-definition"],
+  caseInsensitive: false,
+  mode: "development",
 } as const satisfies LanguageMetaData;
 
 export const CrmscriptImplementationLanguageMetaData = {
-    languageId: 'crmscript',
-    fileExtensions: ['.crmscript'],
-    caseInsensitive: false,
-    mode: 'development'
+  languageId: "crmscript",
+  fileExtensions: [".crmscript"],
+  caseInsensitive: false,
+  mode: "development",
 } as const satisfies LanguageMetaData;
 
-export const CrmscriptGeneratedSharedModule: Module<LangiumSharedCoreServices, LangiumGeneratedSharedCoreServices> = {
-    AstReflection: () => new CrmscriptAstReflection()
+export const CrmscriptGeneratedSharedModule: Module<
+  LangiumSharedCoreServices,
+  LangiumGeneratedSharedCoreServices
+> = {
+  AstReflection: () => new CrmscriptAstReflection(),
 };
 
-export const CrmscriptDefinitionGeneratedModule: Module<LangiumCoreServices, LangiumGeneratedCoreServices> = {
-    Grammar: () => CrmscriptDefinitionGrammar(),
-    LanguageMetaData: () => CrmscriptDefinitionLanguageMetaData,
-    parser: {}
+export const CrmscriptDefinitionGeneratedModule: Module<
+  LangiumCoreServices,
+  LangiumGeneratedCoreServices
+> = {
+  Grammar: () => CrmscriptDefinitionGrammar(),
+  LanguageMetaData: () => CrmscriptDefinitionLanguageMetaData,
+  parser: {},
 };
 
-export const CrmscriptImplementationGeneratedModule: Module<LangiumCoreServices, LangiumGeneratedCoreServices> = {
-    Grammar: () => CrmscriptImplementationGrammar(),
-    LanguageMetaData: () => CrmscriptImplementationLanguageMetaData,
-    parser: {}
+export const CrmscriptImplementationGeneratedModule: Module<
+  LangiumCoreServices,
+  LangiumGeneratedCoreServices
+> = {
+  Grammar: () => CrmscriptImplementationGrammar(),
+  LanguageMetaData: () => CrmscriptImplementationLanguageMetaData,
+  parser: {},
 };
